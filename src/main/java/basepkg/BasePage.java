8de65e8e81c48c5ca9731dfb7053acc09f8767a4
pranaymudi05 <pranaymudi2015@gmail.com>
2019-06-05 18:43:55 +0530

    a

--- src/main/java/basepkg/BasePage.java
+++ src/main/java/basepkg/BasePage.java
@@ -1,9 +1,5 @@
 package basepkg;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeTest;
-
 public class BasePage {
 	
 }
